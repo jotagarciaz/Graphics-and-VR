eada861e39db2416591c7b9ced79ceba67a8959a
Quini Macbook <quini.emancipia@gmail.com>
2019-10-10 23:53:08 +0200

    por si el calentamiento es por esto

---
+++ arcd1.py
@@ -0,0 +1,199 @@
+"""
+Starting Template
+
+Once you have learned how to use classes, you can begin your program with this
+template.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.starting_template
+"""
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Starting Template"
+
+class TextButton:
+    """ Text-based button """
+
+    def __init__(self,
+                 center_x, center_y,
+                 width, height,
+                 text,
+                 font_size=18,
+                 font_face="Arial",
+                 face_color=arcade.color.LIGHT_GRAY,
+                 highlight_color=arcade.color.WHITE,
+                 shadow_color=arcade.color.GRAY,
+                 button_height=2):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.font_size = font_size
+        self.font_face = font_face
+        self.pressed = False
+        self.face_color = face_color
+        self.highlight_color = highlight_color
+        self.shadow_color = shadow_color
+        self.button_height = button_height
+
+    def draw(self):
+        """ Draw the button """
+        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width,
+                                     self.height, self.face_color)
+
+        if not self.pressed:
+            color = self.shadow_color
+        else:
+            color = self.highlight_color
+
+        # Bottom horizontal
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y - self.height / 2,
+                         self.center_x + self.width / 2, self.center_y - self.height / 2,
+                         color, self.button_height)
+
+        # Right vertical
+        arcade.draw_line(self.center_x + self.width / 2, self.center_y - self.height / 2,
+                         self.center_x + self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        if not self.pressed:
+            color = self.highlight_color
+        else:
+            color = self.shadow_color
+
+        # Top horizontal
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y + self.height / 2,
+                         self.center_x + self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        # Left vertical
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y - self.height / 2,
+                         self.center_x - self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        x = self.center_x
+        y = self.center_y
+        if not self.pressed:
+            x -= self.button_height
+            y += self.button_height
+
+        arcade.draw_text(self.text, x, y,
+                         arcade.color.BLACK, font_size=self.font_size,
+                         width=self.width, align="center",
+                         anchor_x="center", anchor_y="center")
+
+    def on_press(self):
+        self.pressed = True
+
+    def on_release(self):
+        self.pressed = False
+
+def check_mouse_press_for_buttons(x, y, button_list):
+    """ Given an x, y, see if we need to register any button clicks. """
+    for button in button_list:
+        if x > button.center_x + button.width / 2:
+            continue
+        if x < button.center_x - button.width / 2:
+            continue
+        if y > button.center_y + button.height / 2:
+            continue
+        if y < button.center_y - button.height / 2:
+            continue
+        button.on_press()
+
+
+def check_mouse_release_for_buttons(x, y, button_list):
+    """ If a mouse button has been released, see if we need to process
+        any release events. """
+    for button in button_list:
+        if button.pressed:
+            button.on_release()
+
+class Canvas(arcade.Window):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+        arcade.set_background_color(arcade.color.BABY_BLUE_EYES)
+        self.myButtons = None
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+
+    def setup(self):
+        # Create your sprites and sprite lists here
+        self.myButtons=[]
+        self.myButtons.append(TextButton(60, 570, 100, 40, "Start", 18,"Arial"))
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+        
+
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        arcade.start_render()
+        for button in self.myButtons:
+            button.draw()
+        # Call draw() on all your sprite lists below
+
+    def update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        http://arcade.academy/arcade.key.html
+        """
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        check_mouse_press_for_buttons(x, y, self.myButtons)
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        check_mouse_release_for_buttons(x, y, self.myButtons)
+
+
+def main():
+    """ Main method """
+    canvas = Canvas(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    canvas.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
--- opengl.py
+++ opengl.py
@@ -6,6 +6,7 @@ import math
 from pyglet_gui.theme import Theme
 from pyglet_gui.buttons import Button
 from pyglet_gui.manager import Manager
+from pyglet_gui.containers import Container
 
 class Window():
     """Initialize the class window, with an array to save points, a default point size of 25, and a window"""
@@ -18,12 +19,69 @@ class Window():
         self.button = False
         self.click_counter = 0
         self.L = []
+        self.option = 0
 
     def get_window(self):
         return self.window
 
     def get_batch(self):
         return self.batch
+
+    def GUI(self):
+        theme = Theme({"font": "Lucida Grande",
+                "font_size": 12,
+                "text_color": [0, 0, 0, 255],
+                "gui_color": [255, 255, 255, 255],
+                "button": {
+                    "down": {
+                        "image": {
+                            "source": "down.png",
+                            "frame": [8, 6, 2, 2],
+                            "padding": [18, 18, 8, 6]
+                        },
+                        "text_color": [220, 220, 220, 255]
+                    },
+                    "up": {
+                        "image": {
+                            "source": "up.png",
+                            "frame": [6, 5, 6, 3],
+                            "padding": [18, 18, 8, 6]
+                        },
+                        "text_color": [0, 0, 0, 255]
+                    }
+                }
+                }, resources_path='theme/')
+
+
+        # just to print something to the console, is optional.
+        def select_line_SI(is_pressed):
+            if is_pressed:
+                self.option = 1
+            else:
+                self.option = 0
+        
+        def select_line_DDA(is_pressed):
+            if is_pressed:
+                self.option = 2
+            else:
+                self.option = 0
+        
+        def select_line_Bresenham(is_pressed):
+            if is_pressed:
+                self.option = 3
+            else:
+                self.option = 0
+                                
+            
+
+        button1 = Button('Line S.I.', on_press=select_line_SI)
+        button2 = Button('Line D.D.A.', on_press=select_line_DDA)
+        button3 = Button('Line Bresenham', on_press=select_line_Bresenham)
+
+       
+        Manager(button1, window=self.window, theme=theme,batch=self.batch,is_movable=True)
+        Manager(button2, window=self.window, theme=theme,batch=self.batch,is_movable=True)
+        Manager(button3, window=self.window, theme=theme,batch=self.batch,is_movable=True)
     #First line drawer algorithm, slope intercept
     def slope_intercept(self, x1, y1, x2, y2):
         self.L.clear()
@@ -33,6 +91,8 @@ class Window():
         y=y1
         m = dy/dx
        
+        Z=self.get_quadrant(x1,x2,y1,y2)
+        print(Z)
         if dy>dx: 
             m=1/m
             b = x1-m*y1
@@ -70,6 +130,9 @@ class Window():
         x=x1+0.5
         y=y1+0.5
         i=0
+
+        Z=self.get_quadrant(x1,x2,y1,y2)
+        print(Z)
         for i in range(0,M+1):
             self.batch.add(1, pyglet.gl.GL_POINTS, None, ('v2i', (math.floor(x), math.floor(y))))
             self.L.append([x,y])
@@ -82,36 +145,44 @@ class Window():
 
         dx = abs(x2 - x1)
         dy = abs(y2 - y1)
-        m = dy/float(dx)
+        m = dy/dx
+        e=m-1/2
         x, y = x1, y1   
         
-        self.batch.add(1, pyglet.gl.GL_POINTS, None, ('v2i', (x, y)))
-        
-        p = 2 * dy - dx
-        
-        if m > 1 :
-            dx, dy = dy, dx
-            x, y = y, x
-            x1, y1 = y1, x1
-            x2, y2 = y2, x2
-        
-        
-
-        
-        self.L.append([x,y])
-
-        for _ in range(2,dx):
-            if p > 0 :
-                y = y + 1 if y < y2 else y-1
-                p = p + 2*(dy-dx)
-            else:
-                p = p + 2*dy
-            
-            x = x + 1 if x < x2 else x - 1
-
-            self.batch.add(1, pyglet.gl.GL_POINTS, None, ('v2i', (x, y)))
+        Z=self.get_quadrant(x1,x2,y1,y2)
+        print(Z)
+        for _ in range(0,dx+1):
             self.L.append([x,y])
-            
+            self.batch.add(1, pyglet.gl.GL_POINTS, None, ('v2i', (x, y)))
+            while e>0 :
+                y = y+1
+                e = e-1
+            x = x+1
+            e = e+m
+  
+        
+    def get_quadrant(self,x1,x2,y1,y2):
+        first_quadrant = False
+        second_quadrant = False
+        third_quadrant = False
+        forth_quadrant = False
+        if y1<y2:
+            if x1<x2:
+                first_quadrant=True
+            if x1>x2:
+                second_quadrant=True
+        if y1>y2:
+            if x1>x2:
+                third_quadrant=True
+            if x1<x2:
+                forth_quadrant=True
+
+        return first_quadrant,second_quadrant,third_quadrant,forth_quadrant
+    
+    
+        
+
+       
     #Function to set the point size
     def point_size(self, size):
         self.PointSize = size
@@ -126,12 +197,6 @@ class Window():
             self.window.clear()
             self.batch.draw()
 
-        @self.window.event
-        def on_key_press(symbol, modifiers):
-            if symbol == key.L:
-                self.button = True
-            else:
-                self.button = False
 
         @self.window.event
         def on_mouse_press(x, y, button, modifiers):
@@ -141,55 +206,24 @@ class Window():
                 self.click_counter += 1
                 self.L.append([x, y]) 
                 if self.click_counter == 2:
-                    self.slope_intercept(self.L[0][0],self.L[0][1],self.L[1][0],self.L[1][1])
+                    if self.option == 1:
+                        self.slope_intercept(self.L[0][0],self.L[0][1],self.L[1][0],self.L[1][1])
+                    if self.option == 2:
+                        self.digital_differential_analyzer(self.L[0][0],self.L[0][1],self.L[1][0],self.L[1][1])
+                    if self.option == 3:
+                        self.bresenham_line_algorithm(self.L[0][0],self.L[0][1],self.L[1][0],self.L[1][1])
                     print(self.L)
                     self.click_counter=0
                     self.L.clear()
             self.batch.draw()
+    
+
 
 window1=Window(300, 300)
 window1.point_size(18)
 window1.action_mouse()
+window1.GUI()
 
 
-theme = Theme({"font": "Lucida Grande",
-               "font_size": 12,
-               "text_color": [0, 0, 0, 255],
-               "gui_color": [255, 255, 255, 255],
-               "button": {
-                   "down": {
-                       "image": {
-                           "source": "down.png",
-                           "frame": [8, 6, 2, 2],
-                           "padding": [18, 18, 8, 6]
-                       },
-                       "text_color": [220, 220, 220, 255]
-                   },
-                   "up": {
-                       "image": {
-                           "source": "up.png",
-                           "frame": [6, 5, 6, 3],
-                           "padding": [18, 18, 8, 6]
-                       },
-                       "text_color": [0, 0, 0, 255]
-                   }
-               }
-              }, resources_path='theme/')
-
-
-# just to print something to the console, is optional.
-def callback(is_pressed):
-    print('Button was pressed to state', is_pressed)
-
-
-
-button = Button('Line', on_press=callback)
-batch = window1.get_batch()
-
-windows=window1.get_window()
-
-
-
-Manager(button, window=windows, theme=theme,batch=batch)
 
 pyglet.app.run()
