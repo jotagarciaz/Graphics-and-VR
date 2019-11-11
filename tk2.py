d99779b032d864b18611cb78c8d5d7fbedbc5d67
jotagarciaz <jgarcia@laisladelfaro.com>
2019-11-11 18:27:00 +0100

    escale funcionando pero moviendose

--- tk2.py
+++ tk2.py
@@ -52,9 +52,27 @@ class Aplicacion:
 		self.dato2=tk.DoubleVar()
 		self.entry2=ttk.Entry(self.lf1, textvariable=self.dato2)
 		self.entry2.grid(column=3, row=1, padx=5, pady=5)
-		##
-		self.boton1=ttk.Button(self.lf1, text="Aplicar cambios", command=self.translate)
-		self.boton1.grid(column=0, row=3, columnspan=2, padx=5, pady=5, sticky="we")
+		
+		# Escalar #
+		self.label3=ttk.Label(self.lf1, text="Escalar")
+		self.label3.grid(column=0,row=2, padx=5, pady=5)
+
+		self.label3=ttk.Label(self.lf1, text="x")
+		self.label3.grid(column=0,row=3, padx=1, pady=1)
+
+		self.dato3=tk.DoubleVar()
+		self.entry3=ttk.Entry(self.lf1, textvariable=self.dato3)
+		self.entry3.grid(column=1, row=3, padx=1, pady=1)
+
+		self.label4=ttk.Label(self.lf1, text="y")
+		self.label4.grid(column=2,row=3, padx=1, pady=1)
+
+		self.dato4=tk.DoubleVar()
+		self.entry4=ttk.Entry(self.lf1, textvariable=self.dato4)
+		self.entry4.grid(column=3, row=3, padx=5, pady=5)
+
+		self.boton1=ttk.Button(self.lf1, text="Aplicar cambios", command=self.escale)
+		self.boton1.grid(column=0, row=5, columnspan=2, padx=5, pady=5, sticky="we")
 		self.entry1.focus()
 
 	def create_l(self,p1,p2):
@@ -141,7 +159,73 @@ class Aplicacion:
 		self.points=np.array(self.L_aux).tolist() 
 
 		self.casa()
+	
+	def escale(self):
+		self.canvas1.delete(tk.ALL)
 		
+		sx = self.dato3.get() 
+		sy = self.dato4.get()
+
+		coordinate_aux = self.points[0]
+		#self.translate(0,0)
+		self.L_aux=np.asarray(self.points)
+		m  = self.L_aux.shape
+		aux = np.ones((m[0],1))
+		aux = np.hstack((self.L_aux,aux))
+		aux1 = np.array([[sx,0,0],[0,sy,0],[0,0,1]])
+		self.L_aux = aux1.dot(np.transpose(aux))
+		self.L_aux = np.transpose(np.delete(self.L_aux,2,0))
+		self.points = np.array(self.L_aux).tolist()
+		#self.translate(coordinate_aux[0],coordinate_aux[1])
+		
+		self.casa()
+
+	def rotate(self, alpha):
+		coordinate_aux = self.L_aux[0]
+		self.translation(0,0)
+		self.L_aux=np.asarray(self.L_aux)
+		m  = self.L_aux.shape
+		aux = np.ones((m[0],1))
+		aux = np.hstack((self.L_aux,aux))
+
+		cos_alpha = math.cos(math.radians(alpha))
+		sen_alpha = math.sin(math.radians(alpha))
+		aux1=np.array([[cos_alpha,sen_alpha,0],[-sen_alpha,cos_alpha,0],[0,0,1]])
+		self.L_aux=aux1.dot(np.transpose(aux))
+		self.L_aux = np.transpose(np.delete(self.L_aux,2,0))
+		self.L_aux=np.array(self.L_aux).tolist()
+		self.translation(coordinate_aux[0],coordinate_aux[1])
+
+	def shearing(self,cx,cy):
+		coordinate_aux = self.L_aux[0]
+		self.translation(0,0)
+		self.L_aux=np.asarray(self.L_aux)
+		m  = self.L_aux.shape
+		aux = np.ones((m[0],1))
+		aux = np.hstack((self.L_aux,aux))
+		aux1=np.array([[1,cx,0],[cy,1,0],[0,0,1]])
+		self.L_aux=aux1.dot(np.transpose(aux))
+		self.L_aux = np.transpose(np.delete(self.L_aux,2,0))
+		self.L_aux=np.array(self.L_aux).tolist()
+		self.translation(coordinate_aux[0],coordinate_aux[1])
+
+	def reflexion(self,reflect_x_or_y):
+		#reflect_x_or_y = true then refleclt x else reflect y
+		coordinate_aux = self.L_aux[0]
+		self.translation(0,0)
+		self.L_aux=np.asarray(self.L_aux)
+		m  = self.L_aux.shape
+		aux = np.ones((m[0],1))
+		aux = np.hstack((self.L_aux,aux))
+		if reflect_x_or_y:
+			aux1=np.array([[1,0,0],[0,-1,0],[0,0,1]])
+		else:
+			aux1=np.array([[-1,0,0],[0,1,0],[0,0,1]])
+
+		self.L_aux=aux1.dot(np.transpose(aux))
+		self.L_aux = np.transpose(np.delete(self.L_aux,2,0))
+		self.L_aux=np.array(self.L_aux).tolist()
+		self.translation(coordinate_aux[0],coordinate_aux[1])
 	   
 
 aplicacion1=Aplicacion()
